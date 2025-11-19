96a3a2dbfffefdcd9142d721c3cc08f7d70dd0b1
Денис Ерилеев <deniserileev85@gmail.com>
2025-11-19 08:14:00 +0300

    Реализованные модели. Фамилия

---
+++ Interface_пр12_Ерилеев/Models/Messages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_пр12_Ерилеев.Models
+{
+    internal class Messages
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Create { get; set; }
+        public int Id_User { get; set; }
+        public Messages() { }
+
+        public Messages(string Message, DateTime Create, int Id_User) { }
+    }
+}
---
+++ Interface_пр12_Ерилеев/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_пр12_Ерилеев.Models
+{
+    internal class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+
+        public Users() { }
+        public Users(int id, string FIO) { }
+    }
+}
