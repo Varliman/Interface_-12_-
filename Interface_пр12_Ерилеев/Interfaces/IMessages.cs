ba5c2b6c4e57ed37e048ed398fe17665d0346dc7
Денис Ерилеев <deniserileev85@gmail.com>
2025-11-19 08:20:19 +0300

    Реализованные интерфейсы. Ерилеев

---
+++ Interface_пр12_Ерилеев/Interfaces/IMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_пр12_Ерилеев.Interfaces
+{
+    internal interface IMessages
+    {
+        void All(out List<IMessages> Messages);
+        void Save(bool Update = false);
+        void Delete();
+    }
+}
---
+++ Interface_пр12_Ерилеев/Interfaces/IUsers.cs
@@ -0,0 +1,14 @@
+﻿using Interface_пр12_Ерилеев.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_пр12_Ерилеев.Interfaces
+{
+    internal interface IUsers
+    {
+        void All(out List<Users> Users);
+    }
+}
