affb03eba1b2be00ade2be2c9115616d2b45776a
Денис Ерилеев <deniserileev85@gmail.com>
2025-11-19 09:31:54 +0300

    Реализованная практическая работа на оценку удовлетворительно. Ерилеев

--- Interface_пр12_Ерилеев/MainWindow.xaml.cs
+++ Interface_пр12_Ерилеев/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Interface_пр12_Ерилеев.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,53 @@ namespace Interface_пр12_Ерилеев
     /// </summary>
     public partial class MainWindow : Window
     {
+        internal UsersContext usersContext = new Classes.UsersContext();
+        internal MessagesContext messagesContext = new MessagesContext();
+        public int IdSelectUser = -1;
+        public static MainWindow mainwindow;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            mainwindow = this;
+
+            LoadUser();
+        }
+
+        public void LoadUser()
+        {
+            foreach(Models.Users user in usersContext.AllUsers)
+                ParentUser.Children.Add(new Elements.Users(user));
+        }
+
+        public void SelectUser(Models.Users User)
+        {
+            if (User != null)
+                IdSelectUser = usersContext.AllUsers.FindIndex(x => x == User);
+
+            ParentMessage.Children.Clear();
+
+            foreach (MessagesContext messages in MessagesContext.AllMessages.FindAll(x => x.IdUser == IdSelectUser))
+                ParentMessage.Children.Add(new Elements.Messages(messages));
+
+            BlockMessage.IsEnabled = true;
+        }
+
+        private void SendMessages(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                if (IdSelectUser == -1)
+                    return;
+
+                MessagesContext newMessages = new MessagesContext(Message.Text, DateTime.Now, IdSelectUser);
+
+                newMessages.Save();
+                Message.Text = "";
+                SelectUser(null);
+            }
+
         }
     }
 }
